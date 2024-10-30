1e180045752c1934bb935b7431011f7fcca56740
Debashis Goswami <145210990+Bittu3487@users.noreply.github.com>
2024-10-30 09:38:12 +0530

    deployement

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Picker from 'emoji-picker-react';
 import { format, isToday, isYesterday } from 'date-fns';
 
 
-const socket = io.connect('https://chat-app-6vfx.vercel.app/?vercelToolbarCode=Hgy3vk0mwEJkdKm');
+const socket = io.connect('https://chat-app-alpha-pied.vercel.app/');
 
 function App() {
     const [username, setUsername] = useState('');
